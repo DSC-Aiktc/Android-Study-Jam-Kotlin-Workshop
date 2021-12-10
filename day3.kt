ff5ad2d958493888f55029be52032d78b6be1dba
bhagwanZaki <zaki.tarveen@gmail.com>
2021-12-10 22:56:56 +0530

    Updated

--- day3.kt
+++ day3.kt
@@ -17,20 +17,35 @@ open class book(val name : String, val author : String,val price: Int){
     }
 }
 
+// inheriting book class
 class manga(name: String,author: String,price: Int,chapter: Int):book(name,author,price){
 	
+    // method overriding
     override fun pricePerQuantity(qty: Int){
         var Tprice = qty*price
         var gst_price = price * 0.18
         println("Final price : ${Tprice+gst_price}")
     }
     
+    // method overloading
+    fun pricePerQuantity(qty: Int, discount : Int){
+        var Tprice = qty*price
+        var gst_price = price * 0.18
+        println("Final price : ${Tprice+gst_price-discount}")
+    }
+    
 }
 
 class school{
+    // secondary constructor
     constructor(name: String,country: String){
         println("Name : $name")
         println("Country : $country")
+    }
+    constructor(name: String,country: String,city: String){
+        println("Name : $name")
+        println("Country : $country")
+        println("City : $city")
     }
 }
 
